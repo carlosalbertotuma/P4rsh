c4cb96770a29f504e162c11a3a6c1dc8a8841dc5
Bl4dSc4n <carlosalbertotuma@yahoo.com.br>
2022-04-16 17:27:19 -0300

    Update Parsh.sh

--- Parsh.sh
+++ Parsh.sh
@@ -1,47 +1,40 @@
 #!/bin/bash
+# Parsh web : Livre uso e modificação
+# Creditos: Bl4dSc4n
+# modo de uso: bash parsh.sh www.vulnweb.com
 
-# Modificiado: Bl4dsc4n
+if [[ $1 == "" ]]; then
+    echo "modo de uso: bash parsh.sh www.vulnweb.com"
+    exit
+else
+    #dominio=$1
+    mkdir $1
+    cd $1
+    wget -q -c $1 -O $1
+    sed -i "s/ /\n/g" $1
+    cat $1 | grep -E "(href=|action=|src=)" | grep -oh '"[^"]*"' | awk -F \" '{print $2}' | sort -u > .tmp-$1
+    cat $1 | grep -E "(href=|action=|src=)" | grep -oh "'[^']*'" | awk -F "'" '{print $2}' | sort -u >> .tmp-$1
+    cat .tmp-$1 | sort -u > achados
 
-echo "dominio"
-read dominio
-mkdir $dominio
-cd $dominio
+    for h in $(cat achados | grep -v "http" | sort -u);
+    do
+        a=$(echo $h | cut -c 1,2)
+        b=$(echo $h | wc -m)
+        if [[ $a =~ // ]]; then
+           c=$(echo $h | cut -d"/" -f 2-$b)  >> links
+           echo $1$c | cut -d " " -f 1
+        elif [[ $a =~ /.* ]]; then
+           echo $1$h | cut -d " " -f 1 >> links
+        else
+           echo $1/$h | cut -d " " -f 1 >> links
+        fi
+    done
 
-wget -q -c $dominio -O $dominio
+    echo -e e "\e[1;32mLinks externos\e[0m"
+    cat achados | grep -Ei "http|https" | grep -v $1 | sort -u | tee -a links-ext-$1
 
-# Filtrando links
-#echo "Quebranco as linhas para melhorar a seleÃ§Ã£o dos links, onde se encontram as palavras 'href' e 'action'."
-sed -i "s/ /\n/g" $dominio
-grep -E "(href=|action=|src=|srcset=)" $dominio > .tmp1-$dominio
-
-#echo "Capturando o conteudo entre aspas e apostrofos."
-grep -oh '"[^"]*"' .tmp1-$dominio > .tmp2-$dominio
-grep -oh "'[^']*'" .tmp1-$dominio >> .tmp2-$dominio
-
-#echo "Removendo as aspas e apostrofos."
-sed -i 's/"//g' .tmp2-$dominio
-sed -i "s/'//g" .tmp2-$dominio
-cat .tmp2-$dominio | sort -u > achados
-
-
-for h in $(cat achados | grep -v "http" | sort -u);
-do
-    a=$(echo $h | cut -c 1,2)
-    b=$(echo $h | wc -m)
-    if [[ $a =~ // ]]; then
-       c=$(echo $h | cut -d"/" -f 2-$b)  >> links
-       echo $dominio$c | cut -d " " -f 1
-    elif [[ $a =~ /.* ]]; then
-       echo $dominio$h | cut -d " " -f 1 >> links
-    else
-       echo $dominio/$h | cut -d " " -f 1 >> links
-    fi
-done
-
-echo -e e "\e[1;32mLinks externos\e[0m"
-cat achados | grep -Ei "http|https" | grep -v $dominio | sort -u | tee -a links-ext-$dominio
-
-echo -e "\e[1;32mSomente links do dominio\e[0m"
-cat achados | grep $dominio > links1
-cat links >> links1
-cat links1 | sort -u | tee -a links-dominio
+    echo -e "\e[1;32mSomente links do dominio\e[0m"
+    cat achados | grep $1 > links1
+    cat links >> links1
+    cat links1 | sort -u | tee -a links-dominio
+fi
